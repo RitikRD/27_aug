fcebc14df4a373d2f57dfe388ae888fa24a7f682
RitikRD <ritikdixit004@gmail.com>
2020-08-26 12:34:02 +0530

    Initial commitv bvnb

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tik_tok_ui/pages/root_app.dart';
+import 'package:tik_tok_ui/screen/splash.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Splash(),
+  ));
+}
+
